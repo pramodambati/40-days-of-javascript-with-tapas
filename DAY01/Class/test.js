d56cd38d1d9e4fd6d182dd7e3597d9cac5a1f105
pramodambati <110030966+pramodambati@users.noreply.github.com>
2025-10-04 03:25:51 +0000

    DAY 1 Class and Task

---
+++ DAY01/Class/test.js
@@ -0,0 +1,3 @@
+console.log("I am a test script");
+
+document.getElementById("someId").innerText = "I am some div" // test.js:3 Uncaught TypeError: Cannot set properties of null (setting 'innerText') at test.js:3:45
