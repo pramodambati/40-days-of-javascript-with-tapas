16269a942fb6a0b28b63d7a0d5957f907ed35476
pramodambati <110030966+pramodambati@users.noreply.github.com>
2025-10-08 02:07:21 +0000

    task complted day 05

--- DAY05/Task/task.js
+++ DAY05/Task/task.js
@@ -16,7 +16,7 @@ for(let i = 1; i <=5; i++) {
     for (let j = 1; j <= i; j++) {
         stars = stars + "*"
     }
-    console.log(stars); 
+    // console.log(stars); 
 }
 
 let nums = ""
@@ -26,7 +26,7 @@ for (let i = 1; i <=5; i++) {
     for (let j = 1; j <=i; j++) {
         nums = nums + j
     }
-    console.log(nums);
+    // console.log(nums);
 }
 
 /*
@@ -39,13 +39,36 @@ Write a program to print the multiplication table of a given number up to 10. Fo
 3 x 10 = 30
 
 */
-
+let N = 3
+for (let i = 1; i <= 10; i++) {
+    // console.log(`${N} * ${i} = ${N*i}`)
+}
 
 /*
 3. Find the summation of all odd numbers between 1 to 500 and print them on the console log.
+*/
+let sum = 0
+
+for (let i = 1; i <= 500; i++) {
+    if (i % 2 != 0) {
+        sum += i
+    }
+}
+// console.log(sum);
+
+/*
 4. Skipping Multiples of 3
 Write a program to print numbers from 1 to 20, but skip multiples of 3.
+*/
 
+for (let i = 1; i <= 20; i++) {
+    if (i % 3 == 0) {
+        continue
+    }
+    // console.log(i);
+}
+
+/*
 5. Reverse Digits of a Number (Using while loop)
 Write a program to reverse the digits of a given number using a while loop.
 
@@ -53,5 +76,35 @@ Example:
 
 Input: 6789
 Output: 9876
+*/
+
+let digit = '6789'
+let reverseDigit = 0
+while (digit) {
+    let rem = digit % 10
+    reverseDigit = reverseDigit * 10 + rem
+    digit = Math.floor(digit/10)
+}
+console.log(reverseDigit);
+console.log(typeof reverseDigit);
+
+
+/*
+ven though you started with '6789' (a string):
+
+% and / force implicit conversion to number
+
+So JS quietly runs numeric operations
+
+And reverseDigit naturally ends up as a number ✅
+*/
+
+/*
 6. Write your understanding on the difefrences between for, while, and do-while loop. Create their flow charts.
+*/
+
+/*
+For loop : When we know the number iterations we need to loop.
+While loop : When we don't the number iterations but we need to break on a condition
+Do-while loop : Execute the code atleast one time before checking the condition.
 */
